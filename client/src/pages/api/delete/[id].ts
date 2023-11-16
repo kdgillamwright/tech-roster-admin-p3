5815b7ab29f5ad9afe24cfb543c56d848e32a06c
Sean Morrow <s3anmorrow@gmail.com>
2023-11-16 13:35:47 -0400

    tweak of request type

--- client/src/pages/api/delete/[id].ts
+++ client/src/pages/api/delete/[id].ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { deleteTechnology } from "@/tools/DataManager";
 
 export default async function handler( request: NextApiRequest, response: NextApiResponse<any>) {
-
-    await deleteTechnology(request, response);
-
+    if (request.method == "POST") {
+        await deleteTechnology(request, response);
+    }
 }
